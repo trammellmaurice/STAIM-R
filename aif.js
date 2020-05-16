21f0b2610c14ca3f1985c5cc8b3d0306e6ca2955
Maurice Trammell <m216762@usna.edu>
2020-05-16 03:31:06 -0400

    Meredith UAV tracks manually operated fighter in 3 dimensions

--- aif.js
+++ aif.js
@@ -15,7 +15,7 @@ const MAX_WIDTH = window.innerWidth;
 const MAX_HEIGHT = window.innerHeight;
 const MAX_ALT = 1500;
 /**********************************************
-            AI CONTROL CONSTANTS
+            uav CONTROL CONSTANTS
 ***********************************************/
 //Thrust
 const THRUST = 1;
@@ -41,7 +41,7 @@ const ALTITUDE_THRESHOLD = 50;
 *********************************************/
 // STEP 1: 2D TRACKING (STATIC ALTITUDE)
 const NUM_INPUTS = 7; //FIGHTER x, y, z, and heading + TARGET x, y, and z
-const NUM_HIDDEN = 35; // IDEA: Probably more hidden layers than the AA Gun?
+const NUM_HIDDEN = 35;
 const NUM_OUTPUTS = 3; //FIGHTER thrust, yaw, climb
 const NUM_SAMPLES = 1000000; //number of samples
 const OUTPUT_THRESHOLD = 0.25; //how close it needs to be to commit to moving
@@ -50,7 +50,6 @@ var nn;
 if(AUTOMATION_ON)
 {
   nn = new NeuralNetwork(NUM_INPUTS, NUM_HIDDEN, NUM_OUTPUTS);
-  // nnAlt = new NeuralNetwork(2, 10, 1);
   let fx, fy, fz, fh, tx, ty, tz;
   for (let i = 0; i < NUM_SAMPLES; i++){
     // random target positions
@@ -69,7 +68,6 @@ if(AUTOMATION_ON)
     alt = changeAlt(tz,fz);
     // train the neural net
     nn.train(normalizeInput(fx,fy,fz,fh,tx,ty,tz), [yaw,thrust,alt]);
-    // nnAlt.train([tz/MAX_ALT,fz/MAX_ALT], [alt])
   }
 }
 /**********************************************
@@ -156,20 +154,18 @@ var environment = {
 *****************************************************************/
 function updateEnvironment(){
   environment.clear();
-  if(fighter.alive || DEV) //fighter alive or developer mode
+  if(uav.alive || DEV) //uav alive or developer mode
   {
     if(AUTOMATION_ON)
     {
-      //TODO: AI CONTROLS
-      let fx = fighter.x;
-      let fy = fighter.y;
-      let fz = fighter.alt;
-      let tx = target.x;
-      let ty = target.y;
-      let tz = target.z;
-      let heading = fighter.heading;
+      let fx = uav.x;
+      let fy = uav.y;
+      let fz = uav.alt;
+      let tx = fighter.x;
+      let ty = fighter.y;
+      let tz = fighter.alt;
+      let heading = uav.heading;
       let predict = nn.feedForward(normalizeInput(fx,fy,fz,heading,tx,ty,tz)).data[0];
-      // let predictAlt = nnAlt.feedForward([tz,fz]).data[0];
       console.log(predict);
       //make movement (left or right)
       let dLeft = Math.abs(predict[0] - LEFT);
@@ -178,34 +174,38 @@ function updateEnvironment(){
       let climb = Math.abs(predict[2] - CLIMB);
       let drop = Math.abs(predict[2] - DROP);
       //compare to OUTPUT_THRESHOLD
-      if(dLeft < OUTPUT_THRESHOLD){fighter.turn(-1);} //turn left
-      else if(dRight < OUTPUT_THRESHOLD){fighter.turn(1);} //turn right
-      if(thrust < OUTPUT_THRESHOLD){fighter.thrust();} // go
-      if(climb < OUTPUT_THRESHOLD){fighter.climb();} //ascend
-      else if(drop < OUTPUT_THRESHOLD){fighter.drop();} //descend
+      if(dLeft < OUTPUT_THRESHOLD){uav.turn(-1);} //turn left
+      else if(dRight < OUTPUT_THRESHOLD){uav.turn(1);} //turn right
+      if(thrust < OUTPUT_THRESHOLD){uav.thrust();} // go
+      if(climb < OUTPUT_THRESHOLD){uav.climb();} //ascend
+      else if(drop < OUTPUT_THRESHOLD){uav.drop();} //descend
     }else{
       //Manual Controls (num pad)
       if(environment.keys && (environment.keys[104] || environment.keys[87])){  //Thrust (8)
-        if(environment.keys && (environment.keys[96] || environment.keys[66])){fighter.burn = true;} //after burner (0)
-        fighter.thrust();
-        if(environment.keys && (environment.keys[107] || environment.keys[9])){fighter.climb();} //Climb requires thrust (+)
+        if(environment.keys && (environment.keys[96] || environment.keys[66])){uav.burn = true;} //after burner (0)
+        uav.thrust();
+        if(environment.keys && (environment.keys[107] || environment.keys[9])){uav.climb();} //Climb requires thrust (+)
       }
-      if(environment.keys && (environment.keys[100] || environment.keys[65])){fighter.turn(-1);} //Turn left (4)
-      if(environment.keys && (environment.keys[102] || environment.keys[68])){fighter.turn(1);} //Turn right (6)
-      if(environment.keys && (environment.keys[109] || environment.keys[16])){fighter.drop();} //Descend (-)
-      if(environment.keys && (environment.keys[101] || environment.keys[83])){fighter.airbrake = true; fighter.drag();} //Brake (5)
+      if(environment.keys && (environment.keys[100] || environment.keys[65])){uav.turn(-1);} //Turn left (4)
+      if(environment.keys && (environment.keys[102] || environment.keys[68])){uav.turn(1);} //Turn right (6)
+      if(environment.keys && (environment.keys[109] || environment.keys[16])){uav.drop();} //Descend (-)
+      if(environment.keys && (environment.keys[101] || environment.keys[83])){uav.airbrake = true; uav.drag();} //Brake (5)
     }
-    // Target Controls
-    if(environment.keys && environment.keys[38]){target.up();}
-    if(environment.keys && environment.keys[40]){target.down();}
-    if(environment.keys && environment.keys[37]){target.lef();}
-    if(environment.keys && environment.keys[39]){target.righ();}
-    if(environment.keys && environment.keys[187]){target.raise();}
-    if(environment.keys && environment.keys[189]){target.lower();}
+    //FIGHTER Manual Controls (num pad)
+    if(environment.keys && (environment.keys[104] || environment.keys[87])){  //Thrust (8)
+      if(environment.keys && (environment.keys[96] || environment.keys[66])){fighter.burn = true;} //after burner (0)
+      fighter.thrust();
+      if(environment.keys && (environment.keys[107] || environment.keys[9])){fighter.climb();} //Climb requires thrust (+)
+    }
+    if(environment.keys && (environment.keys[100] || environment.keys[65])){fighter.turn(-1);} //Turn left (4)
+    if(environment.keys && (environment.keys[102] || environment.keys[68])){fighter.turn(1);} //Turn right (6)
+    if(environment.keys && (environment.keys[109] || environment.keys[16])){fighter.drop();} //Descend (-)
+    if(environment.keys && (environment.keys[101] || environment.keys[83])){fighter.airbrake = true; fighter.drag();} //Brake (5)
   }
+  uav.newPos(); //calculate the new position
   fighter.newPos(); //calculate the new position
+  uav.update(); //update the HUD in the update function
   fighter.update(); //update the HUD in the update function
-  target.update();
 }
 /************************************************************
                         DRAWING
@@ -231,6 +231,182 @@ function drawTarget(ctx, x, y, ang,height){
   ctx.beginPath();
   ctx.arc(0,-60, 30, 0.5*Math.PI, 2.5 * Math.PI);
   ctx.stroke();
+}
+/********************************************************
+                    UAV CLASS
+*********************************************************/
+var uav = {
+  alive : true,
+  counter : 0,
+  flasher : 0,
+  //creating the uav to control
+  width : 15,
+  height : 15,
+  x : MAX_WIDTH/2, //current position
+  y : MAX_HEIGHT/2, //current position
+  vx : 0,
+  vy : 0,
+  thr : 0, //will add to velocity instantly
+  thrx : 0,
+  thry : 0,
+  yaw : 0, //will be positive or negative to adjust angle
+  theta : 0,
+  heading : 0,//for telemetry data
+  fall : 0,
+  lift : 0,
+  momentumx : 0, //can be used to keep the uav moving?
+  momentumy : 0,
+  airbrake : false,
+  burn : false,
+  alt : 500, // should not go to 0 or crash || 0.1 list from thrust -0.1 from drag
+  Target : false,
+  update : function(){
+    // FIXME: UAV doesnt show below 200m
+    ctx = environment.context;
+    if(this.alt <= 200){
+      ctx.fillStyle = '#FFFAB3';
+      ctx.fillRect(0,0,MAX_WIDTH,MAX_HEIGHT);
+    }
+    if(this.alt <= 100){
+      ctx.fillStyle = '#FFCCCC';
+      ctx.fillRect(0,0,MAX_WIDTH,MAX_HEIGHT);
+      if(this.counter < 40){
+        let message = '**__LOW__ALTITUDE__**';
+        if(!this.alive){message = '**__SIMULATION__OVER__**';}
+        ctx.fillStyle = "black";
+        //BOTTOM
+        ctx.fillText(message,20,MAX_HEIGHT-20);
+        ctx.fillText(message,20,MAX_HEIGHT-60);
+        ctx.fillText(message,20,MAX_HEIGHT-100);
+        //TOP
+        ctx.fillText(message,MAX_WIDTH-220,20);
+        ctx.fillText(message,MAX_WIDTH-220,60);
+        ctx.fillText(message,MAX_WIDTH-220,100);
+      }else if(this.counter > 90)
+      {
+        this.counter = 0;
+      }
+      this.counter++;
+    }
+    ctx.save();
+    drawTriangle(ctx, this.x, this.y, this.theta, this.width, this.height);
+    ctx.restore();
+    ctx.save();
+    if(this.target == true)
+    {
+      drawTarget(ctx, this.x,this.y, this.theta, this.height);
+      this.target = false;
+    }
+    ctx.restore();
+    // this.hud(); //No HUD for AI
+    this.drag(); //Apply drag force
+    this.ground(); //check for collision with ground
+  },
+  newPos : function(){
+    this.theta += this.yaw * Math.PI / 180;
+    this.heading += this.yaw; //for telemetry data
+    if(this.heading >= 360){this.heading-=360;}
+    if(this.heading < 0){this.heading+=360;}
+    this.thrx = this.thr * Math.sin(this.theta);
+    this.thry = this.thr * Math.cos(this.theta);
+    if(Math.abs(this.thrx) > 0){this.momentumx = this.thrx;}
+    if(Math.abs(this.thry) > 0){this.momentumy = this.thry;}
+    //check gravity v lift
+    this.gravity();
+    this.alt += this.lift - this.fall;
+    //add momentum
+    if(Math.sqrt(Math.pow(this.vx,2) + Math.pow(this.vy,2)) < 9.9){
+      this.vx += this.momentumx;
+      this.vy += this.momentumy;
+    }
+    //check boundaries to loop
+    if(this.x > MAX_WIDTH){this.x = 0;}
+    if(this.x < 0){this.x = MAX_WIDTH;}
+    if(this.y > MAX_HEIGHT){this.y = 0;}
+    if(this.y < 0){this.y = MAX_HEIGHT;}
+    this.x += this.vx;
+    this.y -= this.vy;
+  },
+  thrust : function(){
+    if(!this.burn){this.thr = 10;}
+    else{this.thr = 17;}
+    this.lift = 5;
+  },
+  turn : function(dir){
+    //change heading of uav
+    if(dir == -1){this.yaw-=5;}
+    else if(dir == 1){this.yaw+=5;}
+  },
+  drag : function(){
+    if(!this.airbrake){
+      //stop changes so they dont go on forever
+      if(this.momentumx > 0){this.momentumx -= 0.1;}
+      else if(this.momentumx < 0){this.momentumx += 0.1;}
+      if(this.momentumy > 0){this.momentumy -= 0.1;}
+      else if(this.momentumy < 0){this.momentumy += 0.1;}
+    }
+    else{
+      this.momentumx = 0;
+      this.momentumy = 0;
+    }
+    this.lift = 0;
+    this.fall = 0;
+    this.vx = 0;
+    this.vy = 0;
+    this.thr = 0;
+    this.yaw = 0; //set yaw back to 0 after the movement
+    this.ground();
+  },
+  laze : function(){
+    this.target = true;
+  },
+  gravity : function() {
+    if(this.alt>0){this.fall += 5;}
+    else{this.alt = 0;}
+  },
+  climb : function() {
+    if(this.alt <= MAX_ALT)
+    {
+      this.lift += 8;
+    } else{
+      this.alt = MAX_ALT;
+    }
+  },
+  drop : function() {
+    this.fall += 8;
+  },
+  ground : function() {
+    //check if ai is too low
+    if(this.alt <= 0 )
+    {
+      if(!DEV && this.alive){
+        this.alive = false;
+        console.log('crash');
+      }
+      this.alt = 0;
+      this.momentumx = 0;
+      this.momentumy = 0;
+    }
+  },
+  //HUD telemetry indication
+  //--------------------------------------------
+  hud : function(){
+    ctx = environment.context;
+    ctx.fillStyle = "black";
+    ctx.strokeStyle = "black";
+    //update telemetry
+    ctx.fillText('x:'+this.x.toFixed().toString()+'  y:'+this.y.toFixed().toString()+'  alt:'+this.alt.toFixed().toString()+'m',20,20);
+    ctx.fillText('vx:'+this.vx.toFixed().toString()+'m/s'+'  vy:'+this.vy.toFixed().toString()+'m/s'+'  heading:'+uav.heading.toFixed().toString()+'°', 20, 60);
+    ctx.fillText('thr:'+this.thr.toFixed().toString()+'m/sE2'+'  climb:'+(this.lift-this.fall).toFixed().toString()+ 'm/s'+'  yaw:'+this.yaw.toFixed().toString()+'°',20,100);
+    ctx.fillText('mox:'+this.momentumx.toFixed().toString()+'m/s'+'  moy:'+this.momentumy.toFixed().toString()+'m/s',20,140);
+    var brake;
+    if (this.airbrake){brake = 'engaged';}else{brake = 'disengaged';}this.airbrake = false;
+    ctx.fillText('RB: '+brake,20,180);
+    if(this.burn){ctx.fillText('burner: engaged',100,180);}else{ctx.fillText('burner: disengaged',100,180);}this.burn = false;
+    ctx.beginPath();
+    ctx.rect(10, 10, 230, 220);
+    ctx.stroke();
+  }
 }
 /********************************************************
                     FIGHTER CLASS
@@ -407,38 +583,3 @@ var fighter = {
     ctx.stroke();
   }
 }
-/**********************************************************
-                      TARGET CLASS
-***********************************************************/
-var target = {
-  x : MAX_WIDTH,
-  y : 20,
-  z : 0,
-  update : function() {
-    //check boundaries to loop
-    if(this.x > MAX_WIDTH){this.x = 0;}
-    if(this.x < 0){this.x = MAX_WIDTH;}
-    if(this.y > MAX_HEIGHT){this.y = 0;}
-    if(this.y < 0){this.y = MAX_HEIGHT;}
-    this.drawTarget();
-  },
-    righ : function(){this.x+=TRACK_SPEED;},
-    lef : function(){this.x-=TRACK_SPEED;},
-    up : function(){this.y-=TRACK_SPEED;},
-    down : function(){this.y+=TRACK_SPEED;},
-    raise : function(){this.z+=RAISE_SPEED;},
-    lower : function(){this.z-=RAISE_SPEED;},
-    drawTarget : function(){
-      ctx = environment.context;
-      ctx.moveTo(this.x,this.y);
-      if(Math.abs(fighter.alt - this.z) >= ALTITUDE_THRESHOLD){
-        ctx.strokeStyle = "gray";
-      }
-      else{
-        ctx.strokeStyle = "#FF0000";
-      }
-      ctx.beginPath();
-      ctx.arc(this.x,this.y, TARGET_SIZE, 0.5*Math.PI, 2.5 * Math.PI);
-      ctx.stroke();
-    }
-  }
